54b690f5afb3bbe0869008d86248c39f59db6c0d
Jennifer Huestis <jenrhues@ut.utm.edu>
2020-03-10 14:33:21 -0500

    A good start...

--- Threading/Program.cs
+++ Threading/Program.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿/*
+ * Name: Jennifer Huestis
+ * Date: 03/12/2020
+ * File: Program.cs
+ * Description: 
+ */
+
+using System;
 
 namespace Threading
 {
@@ -6,7 +13,32 @@ namespace Threading
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
         }
     }
+
+    class FindPiThread
+    {
+        int dartsToThrow;
+        int countDarts;
+        Random rand;
+        FindPiThread(int dartsToThrow)
+        {
+            this.dartsToThrow = dartsToThrow;
+            countDarts = 0;
+            rand = new Random();
+        }
+        public int GetDartCount()
+        {
+            return countDarts;
+        }
+        public void throwDarts()
+        {
+            for (int i = 0; i < dartsToThrow; i++)
+            {
+
+            }
+        }
+    }
+
 }
