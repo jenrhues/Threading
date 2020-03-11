8448a1aa9c5d107ee53eb2f4704fc3ff421d84ee
Jennifer Huestis <jenrhues@ut.utm.edu>
2020-03-10 20:50:13 -0500

    Much programming! Much wow!

--- Threading/Program.cs
+++ Threading/Program.cs
@@ -6,14 +6,47 @@
  */
 
 using System;
+using System.Collections.Generic;
+using System.Threading;
 
 namespace Threading
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            //Console.WriteLine("Hello World!");
+        {            
+            Console.WriteLine("Welcome to Jennifer's Amazing Pi Calculator!\nHere, you will throw darts at 1/4 of a dart board" +
+                " for a specified number of threads to calculate Pi.\nHow many darts would you like to throw?");
+            int dartTotal = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("How many threads would you like to run?");
+            int threadTotal = Int32.Parse(Console.ReadLine());
+
+            List<Thread> thread = new List<Thread>();
+            List<FindPiThread> findPiThread = new List<FindPiThread>();
+
+            Console.WriteLine("Running calculations...\n");
+            for (int i = 0; i < threadTotal; i++) {
+                FindPiThread piSimulation = new FindPiThread(dartTotal);
+                findPiThread.Add(piSimulation);
+                Thread piThread = new Thread(new ThreadStart(piSimulation.throwDarts));
+                thread.Add(piThread);
+                piThread.Start();
+                Thread.Sleep(16);
+            }
+            for (int i = 0; i < thread.Count; i++)
+            {
+                thread[i].Join();
+            }
+            int dartCount = 0;
+            for (int i = 0; i < findPiThread.Count; i++)
+            {
+                dartCount += findPiThread[i].GetDartCount();
+            }
+            double Pi = 4.0 * (dartCount / Convert.ToDouble(dartTotal * threadTotal));
+            Console.WriteLine("Your Pi estimation is " + Pi + " for " + dartTotal + " darts in " + threadTotal + " threads!\n");
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey(true);
         }
     }
 
@@ -22,7 +55,7 @@ namespace Threading
         int dartsToThrow;
         int countDarts;
         Random rand;
-        FindPiThread(int dartsToThrow)
+        public FindPiThread(int dartsToThrow)
         {
             this.dartsToThrow = dartsToThrow;
             countDarts = 0;
@@ -36,7 +69,13 @@ namespace Threading
         {
             for (int i = 0; i < dartsToThrow; i++)
             {
-
+                double X = rand.NextDouble();
+                double Y = rand.NextDouble();
+                double hypotenuse = Math.Sqrt(Math.Pow(X, 2) + Math.Pow(Y, 2));
+                if (hypotenuse <= 1.0)
+                {
+                    countDarts++;
+                }
             }
         }
     }
